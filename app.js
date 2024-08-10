cff0aa75be58119dc4d2afe702e8f73323879182
Sander <hosin8388@gmail.com>
2024-08-10 12:30:02 +0330

    created the gitignore file

---
+++ app.js
@@ -0,0 +1,11 @@
+const login = () => {
+    return true;
+}
+const register = () =>{
+    return true;
+}
+const undo = () => {
+    return false;
+}
+const userName = 'Sander';
+const userAge = 22;
